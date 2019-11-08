5101d8d92c21a326294cdb339471b00ca040ad43
Colman Currie <curriecolman@gmail.com>
2019-11-08 10:15:34 -0800

    begin the pizza func

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
+// User Logic---------
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
 
+Pizza.prototype.order = function {
+  var pieCost = 12;
+  if (this.toppings.includes("chicken")) {
+    pieCost += 2;
+  };
+  if (this.toppings.includes("bacon")) {
+    pieCost += 2;
+  };
+  if (this.toppings.inludes("Olives", "Artichoke hearts")) {
+    pieCost += 1;
+  };
+  if (this.size === 1) {
+    pieCost += 3;
+
+  }
+
+}
 
 
 
@@ -25,3 +47,6 @@
 $(document).ready(function() {
     $("form#pizzaOrder").submit(function(event) {
         event.preventDefault();
+
+    })
+  });
