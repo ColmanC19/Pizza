378f50baf0347fed4c31a8b1a5b5e59d69452f71
Colman Currie <curriecolman@gmail.com>
2019-11-08 12:45:52 -0800

    still check for bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
 // User Logic---------
-function Pizza(size, toppings) {
+function Pizza(size, topping1, topping2) {
   this.size = size;
-  this.toppings = toppings;
+  this.topping1 = topping1;
+  this.topping2 = topping2;
 }
 
 Pizza.prototype.orderPrice = function() {
   var pieCost = 12;
-  if (this.size === "L") {
+  if (this.size === "18 inches") {
     pieCost += 3;
   }
-   if (this.size === "XL") {
+   if (this.size === "24 inches") {
     pieCost += 6;
   }
-  if (this.toppings.includes("Chicken", "Bacon")) {
+  if (this.topping1.includes("Chicken", "Bacon")) {
     pieCost += 2;
   }
 
-  if (this.toppings.inludes("Olives", "Artichoke hearts", "Onions")) {
+  if (this.topping2.inludes("Olives", "Artichoke hearts", "Onions")) {
     pieCost += 1;
   };
 
@@ -25,15 +26,17 @@ Pizza.prototype.orderPrice = function() {
 };
 
 
+
 // business logic
 $(document).ready(function() {
     $("form#pizzaOrder").submit(function(event) {
         event.preventDefault();
+        console.log(alert("hi"));
         var pizzaSize = $("#pizzaSize").val();
         var topping1 = $("#topping1").val();
         var topping2 = $("#topping2").val();
         var pieOrder = new Pizza(pizzaSize, topping1, topping2)
         pieOrder.orderPrice();
-        $("p#result").text("Thank you for your order. Your " + pieOrder.pizzaSize + "with " + pieOrder.topping1 + pieOrder.topping2 + "Your total is: " + pieOrder.pieCost)
+        $("#result").text("Thank you for your order. Your " + pieOrder.pizzaSize + "with " + pieOrder.topping1 + pieOrder.topping2 + "Your total is: " + pieOrder.pieCost)
     })
   });
